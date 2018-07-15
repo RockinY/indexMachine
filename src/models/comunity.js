8a484390be64b070d043dca1b3b9ab2fc406a0ba
Bran Liang <lby89757@hotmail.com>
2018-07-15 13:05:29 +0800

    Add community model

--- src/models/comunity.js
+++ src/models/comunity.js
@@ -7,9 +7,67 @@ import {
   listenToChangedFieldIn,
 } from '../utils/changeFeed'
 import db from '../utils/db'
+import Raven from '../utils/raven';
 
 export const newCommunity = () => {
   return listenToNewDocumentsIn(db, 'communities', data => {
     const searchableCommunity = dbCommunityToSearchCommunity(data)
+    return client.create({
+      index: 'communities',
+      type: 'item',
+      id: searchableCommunity.id,
+      body: searchableCommunity
+    })
+    .then(() => {
+      debug('stored new community in search')
+      return
+    })
+    .catch(err => {
+      debug('error indexing a community')
+      console.log(err)
+      Raven.captureException(err)
+    })
+  })
+}
+
+export const deletedCommunity = () => {
+  listenToDeletedDocumentsIn(db, 'communities', data => {
+    return client.delete({
+      index: 'communities',
+      type: 'item',
+      id: data.id
+    })
+    .then(() => {
+      debug('deleted community in search')
+      return
+    })
+    .catch(err => {
+      debug('error deleting a community')
+      console.log(err)
+      Raven.captureException(err)
+    })
+  })
+}
+
+export const editedCommunity = () => {
+  listenToChangedFieldIn(db, 'modifiedAt')('communities', data => {
+    const searchableCommunity = dbCommunityToSearchCommunity(data)
+    return client.update({
+      index: 'communities',
+      type: 'item',
+      id: data.id,
+      body: {
+        doc: searchableCommunity
+      }
+    })
+    .then(() => {
+      debug('stored edited community in search');
+      return
+    })
+    .catch(err => {
+      debug('error updating a community')
+      console.log(err)
+      Raven.captureException(err)
+    })
   })
 }
